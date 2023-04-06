e998dac1fa289ff522f0572b9a6ca4a9c0379cc9
Rafael Morales <rmorales@utem.cl>
2023-04-06 15:28:40 -0400

    Added flutter commands
    No me gusta mucho como esta hecho, pero funciona.

--- plugins/user.lua
+++ plugins/user.lua
@@ -21,4 +21,20 @@ return {
       { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
     },
   },
+  {
+    "akinsho/flutter-tools.nvim", -- add lsp plugin
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    keys = {
+      { "<leader>F", desc = "Flutter Tools" },
+      { "<leader>Fs", "<cmd>FlutterRun<cr>", desc = "Run the current flutter project" },
+      { "<leader>Fx", "<cmd>FlutterQuit<cr>", desc = "Ends a running session" },
+      { "<leader>Fr", "<cmd>FlutterReload<cr>", desc = "Reloads the running project." },
+      { "<leader>FR", "<cmd>FlutterRestart<cr>", desc = "Restarts the current project" },
+      { "<leader>Fd", "<cmd>FlutterDevices<cr>", desc = "Brings up list of connected devices." },
+    },
+  },
 }
